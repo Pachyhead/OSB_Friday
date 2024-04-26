81c6903708475faf0e706df6b85a3ec9db43cdd4
temraire117 <temraire117@gmail.com>
2024-04-26 22:23:31 +0900

    충북대 생활협동조합 사이트를 스크래핑하여 tmp폴더에  아침, 점심, 저녁 메뉴를 각각 result1.txt, result2.txt, result3.txt 로 저장하는 Scraping class 구현 (MainActivity에는 아직 import만 되어있는 상태)

--- OSB_Friday_Main_UI/app/src/main/java/com/example/main_ui/MainActivity.java
+++ OSB_Friday_Main_UI/app/src/main/java/com/example/main_ui/MainActivity.java
@@ -12,12 +12,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.main_ui.Scraping; // 스크레이핑 import
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
     }
 }
