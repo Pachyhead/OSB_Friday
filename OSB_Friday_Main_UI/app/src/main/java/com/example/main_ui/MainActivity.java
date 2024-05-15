500c1c9dd4f715739079d40616b6e171a2aac532
Pachyhead <bitcoin-groove0a@icloud.com>
2024-05-16 02:11:02 +0900

    add button to Week Menu UI on Main UI

--- OSB_Friday_Main_UI/app/src/main/java/com/example/main_ui/MainActivity.java
+++ OSB_Friday_Main_UI/app/src/main/java/com/example/main_ui/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        ImageButton btnToWeekMenu = findViewById(R.id.btn_to_Week_Menu);
+        btnToWeekMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WeekMenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
