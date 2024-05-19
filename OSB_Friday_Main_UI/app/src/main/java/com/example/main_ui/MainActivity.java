bc57a327011b244a98605e8fb66d1c063d810ecf
Acsard <temraire117@gmail.com>
2024-05-19 17:37:33 +0900

    스크레이핑 결과를 ui에 출력, 일단 아침 결과만 출력

--- OSB_Friday_Main_UI/app/src/main/java/com/example/main_ui/MainActivity.java
+++ OSB_Friday_Main_UI/app/src/main/java/com/example/main_ui/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.main_ui;
 
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +15,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.main_ui.Scraping; // 스크레이핑 import
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Scraping.OnScrapingCompleteListener{
 
 
     @Override
@@ -23,6 +25,24 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        Scraping task = new Scraping(this);
+        task.execute();
 
     }
+    @Override
+    public void onScrapingComplete(String[] weeks) {
+        // 스크래이핑 결과를 ui에 출력
+        TextView monday = findViewById(R.id.text_monday);
+        monday.setText(weeks[0]);
+        TextView tuesday = findViewById(R.id.text_tuesday);
+        tuesday.setText(weeks[1]);
+        TextView wednesday = findViewById(R.id.text_wednesday);
+        wednesday.setText(weeks[2]);
+        TextView thursday = findViewById(R.id.text_thursday);
+        thursday.setText(weeks[3]);
+        TextView friday = findViewById(R.id.text_friday);
+        friday.setText(weeks[4]);
+    }
+
 }
+
--- OSB_Friday_Main_UI/settings.gradle.kts
+++ OSB_Friday_Main_UI/settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "Main_UI"
 include(":app")
- 
