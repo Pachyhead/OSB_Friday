cf4f1293b36f33ef32d601b08c7fc5c340de62af
Pachyhead <bitcoin-groove0a@icloud.com>
2024-05-31 03:01:35 +0900

    유저 정보 설정 버튼과 설정 UI 생성

--- OSB_Friday_Main_UI/app/src/main/java/com/example/main_ui/MainActivity.java
+++ OSB_Friday_Main_UI/app/src/main/java/com/example/main_ui/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.main_ui;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,6 +34,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, WeekMenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton btnUserSetting = findViewById(R.id.btn_user_setting);
+        btnUserSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ActivityUserInfo.class);
                 startActivity(intent);
             }
         });
--- OSB_Friday_Main_UI/app/src/main/java/com/example/main_ui/WeekMenuActivity.java
+++ OSB_Friday_Main_UI/app/src/main/java/com/example/main_ui/WeekMenuActivity.java
@@ -1,6 +1,9 @@
 package com.example.main_ui;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class WeekMenuActivity extends AppCompatActivity {
+
+    private ListView[] weekMenuListArr = new ListView[5];
+    private int[] menuListViewIds = {R.id.Weekly_Menu_Monday_list,
+                                 R.id.Weekly_Menu_Tuesday_list,
+                                 R.id.Weekly_Menu_Wednesday_list,
+                                 R.id.Weekly_Menu_Thursday_list,
+                                 R.id.Weekly_Menu_Friday_list};
+    private ArrayAdapter<String>[] menuAdapters = new ArrayAdapter[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,25 @@ public class WeekMenuActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        String[][] menuDatabase = new String[5][0];
+
+        for(int i = 0; i < 5; i++) {
+            weekMenuListArr[i] = findViewById(menuListViewIds[i]);
+            menuAdapters[i] = new ArrayAdapter<>(this,
+                    android.R.layout.simple_list_item_1, menuDatabase[i]);
+            weekMenuListArr[i].setAdapter(menuAdapters[i]);
+        }
+
+        //updateMenuListData
+    }
+
+    private void updateMenuListData(int index, String[] newData) {
+        if (index >= 0 && index < menuAdapters.length) {
+            ArrayAdapter<String> adapter = menuAdapters[index];
+            adapter.clear();
+            adapter.addAll(newData);
+            adapter.notifyDataSetChanged();
+        }
     }
 }
