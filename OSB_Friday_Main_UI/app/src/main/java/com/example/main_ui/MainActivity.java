565f3aaba3760a85fe48680d8efbdebe9a33de33
ImagoDei0212 <imagodei0212@gmail.com>
2024-06-07 01:40:12 +0900

    MainActivity.java/activity_main.xml/activity_user_info.xml/activity_week_menu.xml 자잘한 오류 해결

--- OSB_Friday_Main_UI/app/src/main/java/com/example/main_ui/MainActivity.java
+++ OSB_Friday_Main_UI/app/src/main/java/com/example/main_ui/MainActivity.java
@@ -1,29 +1,15 @@
 package com.example.main_ui;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CalendarView;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
-import com.example.main_ui.Scraping; // 스크레이핑 import
 
 public class MainActivity extends AppCompatActivity {
 
